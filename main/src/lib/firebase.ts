8dba593be9b9de47e96c3c9a871f5e644cb9e1be
Dkill <73032415+DkillG@users.noreply.github.com>
2024-09-24 23:14:09 -0300

    feat: setup firebase

---
+++ main/src/lib/firebase.ts
@@ -0,0 +1,11 @@
+import { config } from "@/config/firebase";
+import { initializeApp } from "firebase/app";
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
+
+const firebase = initializeApp(config);
+
+const storage = getStorage(firebase);
+const database = getFirestore(firebase);
+
+export { storage, database };
