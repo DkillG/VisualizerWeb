c77cf35fb0cba74e25661cd64739393278e3318b
Dkill <73032415+DkillG@users.noreply.github.com>
2024-09-25 20:20:20 -0300

    feat: function to get points

---
+++ main/src/functions/point.ts
@@ -0,0 +1,23 @@
+import { PointDTO } from '@/types/point';
+import { database } from '@/lib/firebase';
+import { collection, getDocs } from 'firebase/firestore';
+
+export const getPoints = async (): Promise<PointDTO[]> => {
+	try {
+		const query = await getDocs(collection(database, 'points'));
+		const points: PointDTO[] = [];
+
+		query.forEach(doc => {
+			const data = doc.data();
+			points.push({
+				name: data.name,
+				coords: [data.coordX, data.coordY],
+			});
+		});
+
+		return points;
+	} catch (error) {
+        console.error(error);
+        return [];
+    }
+};
