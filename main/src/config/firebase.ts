a51cd47e0ed7eddb955f46b0cf8d3e22f0a927ff
Dkill <73032415+DkillG@users.noreply.github.com>
2024-09-24 23:01:54 -0300

    feat: Added firebase

---
+++ main/src/config/firebase.ts
@@ -0,0 +1,8 @@
+export const config = {
+	appId: process.env.FIREBASE_APP_ID,
+	apiKey: process.env.FIREBASE_API_KEY,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+};
