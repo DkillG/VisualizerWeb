55ca009f76da69326d4792e2e2441792fcde69cd
Dkill <73032415+DkillG@users.noreply.github.com>
2024-09-25 20:23:26 -0300

    feat: function to get materials

---
+++ main/src/functions/material.ts
@@ -0,0 +1,25 @@
+import { database } from '@/lib/firebase';
+import { MaterialDTO } from '@/types/material';
+import { collection, getDocs } from 'firebase/firestore';
+
+export const getMaterials = async (): Promise<MaterialDTO[]> => {
+	try {
+		const query = await getDocs(collection(database, 'materials'));
+		const materials: MaterialDTO[] = [];
+
+		query.forEach(doc => {
+			const data = doc.data();
+			materials.push({
+				name: data.name,
+				points: data.points,
+				layers: data.layers,
+				preview: data.preview,
+			});
+		});
+
+		return materials;
+	} catch (error) {
+        console.error(error);
+        return [];
+    }
+};
